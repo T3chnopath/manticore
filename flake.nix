cf8662d94f8cba0f525feec70dc6837f3d03c111
Technopath <josias.navasca@gmail.com>
2023-08-22 19:39:32 -0700

    Initial nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "OpenTechLab C++ Application Example";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgsUnstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs { inherit system; };
+        pkgsUnstable = import nixpkgsUnstable { inherit system; };
+      in {
+        packages = rec {
+          default = pkgs.stdenv.mkDerivation {
+            name = "manticore";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+            ];
+
+            buildInputs = with pkgs; [
+              python39
+            ];
+          };
+
+          dockerImage = pkgsUnstable.dockerTools.buildNixShellImage {
+            tag = "latest";
+            drv = default.overrideAttrs (old: { src = null; });
+          };
+        };
+      });
+}
