08c3beefa480d1c71e8ade91f895bcb20703b502
Technopath <josias.navasca@gmail.com>
2023-08-23 20:06:24 -0700

    Set ThreadX to not be a flake

--- flake.nix
+++ flake.nix
@@ -3,28 +3,39 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
-    nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     threadx.url = "github:azure-rtos/threadx";
+    threadx.flake = false;
   };
 
-  outputs = { self, nixpkgs, nixpkgsUnstable, flake-utils, threadx }:
+  outputs = { self, nixpkgs, flake-utils, threadx }:
     flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = import nixpkgs { inherit system; };
-        pkgsUnstable = import nixpkgsUnstable { inherit system; };
-      in {
-        packages = rec {
-          default = pkgs.stdenv.mkDerivation {
-            name = "manticore";
-            src = ./.;
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages = rec {
+        default = pkgs.stdenv.mkDerivation {
+          name = "manticore";
+          src = ./.;
 
-            buildInputs = with pkgs; [
-              python39
-              threadx
-              cmake
-            ];
-          };
+          # Assuming you want to build ThreadX too, you can include it as a source
+          nativeBuildInputs = [ pkgs.makeWrapper ]; # or other build tools you might need
+
+          # You can specify the source path to the ThreadX repository
+          buildInputs = with pkgs; [
+            python39
+          ];
+
+          buildPhase = ''
+            mkdir -p $out
+            cp -r $src $out/src  # Copy ThreadX source to the output
+            # Your build commands here
+          '';
+
+          installPhase = ''
+            # Your install commands here
+          '';
 
         };
-      });
+      };
+    });
 }
