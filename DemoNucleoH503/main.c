eae7d6e9f6e485c221c93b9874925ae55a57e619
T3chnopath <josias.navasca@gmail.com>
2023-09-11 20:16:09 -0700

    Update main.c example

--- DemoNucleoH503/main.c
+++ DemoNucleoH503/main.c
@@ -1,4 +1,13 @@
 #include "bsp_nucleo_h503.h"
+#include "tx_api.h"
+#include "stdbool.h"
+
+#define THREAD_STACK_SIZE 1024
+
+uint8_t thread_stack[THREAD_STACK_SIZE];
+TX_THREAD thread_ptr;
+
+void my_thread_entry(ULONG ctx);
 
 int main(void)
 {
@@ -10,10 +19,32 @@ int main(void)
 
     /* Initialize all configured peripherals */
     BSP_GPIO_Init();
-  
-    while (1)
+
+    tx_kernel_enter();
+   }
+
+void tx_application_define(void *first_unused_memory)
+{
+    /* Create my_thread! */
+    tx_thread_create( &thread_ptr, 
+                     "my_thread", 
+                      my_thread_entry, 
+                      0x1234, 
+                      first_unused_memory, 
+                      THREAD_STACK_SIZE, 
+                      4,
+                      4, 
+                      1, 
+                      TX_AUTO_START);
+
+}
+
+void my_thread_entry(ULONG initial_input)
+{
+    while( true )
     {
         HAL_Delay(1000);
         HAL_GPIO_TogglePin(LED1_GREEN_GPIO_Port, LED1_GREEN_Pin);
     }
+
 }
