05677b7ac3064105c5bfc23fe79e3c40a4a0eb08
T3chnopath <josias.navasca@gmail.com>
2023-09-30 01:27:44 -0700

    Adjust LED blink to check full payload

--- DemoNucleoH503/main.c
+++ DemoNucleoH503/main.c
@@ -9,13 +9,13 @@
 uint8_t thread_stack[THREAD_STACK_SIZE];
 TX_THREAD thread_ptr;
 
-static uint8_t mcanTxData[64] = {  177, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+static uint8_t mcanTxData[64] = {  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                             11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                             21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
                             31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
                             41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
                             51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-                            61, 62, 63, 64 };
+                            61, 62, 63 };
 
 
 static sMCAN_Message mcanRxMessage;
@@ -68,7 +68,7 @@ void my_thread_entry(ULONG initial_input)
     {
         HAL_Delay(1000);
        
-        MCAN_TX( MCAN_EMERGENCY, LOG, ALL_DEVICES, mcanTxData);
+        MCAN_TX( MCAN_EMERGENCY, LOG, MAIN_COMPUTE, mcanTxData);
 
     }
 
@@ -76,13 +76,28 @@ void my_thread_entry(ULONG initial_input)
 
 bool MCAN_Rx_Handler( void )
 {
-    if ( mcanRxMessage.mcanID.MCAN_RX_Device == ALL_DEVICES)
+    bool dataFlag = true; 
+    if ( mcanRxMessage.mcanID.MCAN_RX_Device == MAIN_COMPUTE)
     {
-        if ( mcanRxMessage.mcanData[0] == 177 )
+        for ( uint8_t i = 0; (i < 64 && dataFlag); i++ )
+        {
+            if ( mcanRxMessage.mcanData[i] != i )
+            {
+                dataFlag = false;
+            }
+        }
+
+        if ( dataFlag )
         {
             HAL_GPIO_TogglePin(LED1_GREEN_GPIO_Port, LED1_GREEN_Pin);
             return true;
         }
+
+        else
+        {
+            return false;
+        }
+            
     } 
 
     return false;
