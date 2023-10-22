14287a424704915498ce780d5a4d7514a1945cd9
T3chnopath <josias.navasca@gmail.com>
2023-10-21 18:30:52 -0700

    BSP changes

--- DemoNucleoH503/main.c
+++ DemoNucleoH503/main.c
@@ -21,19 +21,10 @@ int main(void)
     /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
     HAL_Init();
 
-    /* Configure the system clock */
-    BSP_SystemClock_Config();
-    
-    /* Initialize Peripheral Clocks */
-    BSP_PeriphClock_Config();
+    /* Initialize BSP */
+    BSP_Init();
 
-    /* Initialize all configured peripherals */
-    BSP_GPIO_Init();
-
-    MCAN_PeriphConfig( FDCAN1_PERIPH, DEV_MAIN_COMPUTE);
-    MCAN_RegisterRX_Buf(&mcanRxMessage);
-    MCAN_StartRX_IT();
-
+    MCAN_Init( FDCAN1, DEV_MAIN_COMPUTE, &mcanRxMessage );
 
     tx_kernel_enter();
    }
@@ -55,6 +46,7 @@ void tx_application_define(void *first_unused_memory)
 
 void thread_main(ULONG ctx)
 {
+    MCAN_SetEnableIT(MCAN_ENABLE);
     bool heartbeatFlagPrevious = false;
 
     while( true )
