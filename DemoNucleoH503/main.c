9bcbdc8975e6c469c16cc41d060f72efea6f75df
T3chnopath <josias.navasca@gmail.com>
2023-11-27 00:08:49 -0800

    bsp init changes

--- DemoNucleoH503/main.c
+++ DemoNucleoH503/main.c
@@ -4,16 +4,24 @@
 
 
 // Main Thread
-#define THREAD_MAIN_STACK_SIZE 512
+#define THREAD_MAIN_STACK_SIZE 1024
 static TX_THREAD stThreadMain;
 static uint8_t auThreadMainStack[THREAD_MAIN_STACK_SIZE];
-
-static bool heartbeatFlag = false;
-static sMCAN_Message mcanRxMessage = { 0 };
-
-static uint8_t heartbeatData[] = { 0xDE, 0xCA, 0XF, 0xC0, 0xFF, 0xEE, 0xCA, 0xFE};
-
+static const uint8_t THREAD_MAIN_DELAY_MS = 10;
 void thread_main(ULONG ctx);
+
+// Blink Thread
+#define THREAD_BLINK_STACK_SIZE 512
+static TX_THREAD stThreadBlink;
+static uint8_t auThreadBlinkStack[THREAD_BLINK_STACK_SIZE];
+static const uint16_t THREAD_BLINK_DELAY_MS = 1000;
+void thread_blink(ULONG ctx);
+
+static sMCAN_Message mcanRxMessage = { 0 };
+static const uint16_t HEARTBEAT_DELAY_MS = 1000;
+static uint8_t heartbeatData[] = {0xDE, 0xCA, 0xFF, 0xC0, 0xFF, 0xEE, 0xCA, 0xFE};
+static bool heartbeatFlag = false;
+
 
 int main(void)
 {
@@ -34,9 +42,21 @@ void tx_application_define(void *first_unused_memory)
                       0, 
                       auThreadMainStack, 
                       THREAD_MAIN_STACK_SIZE, 
-                      4,
-                      4, 
-                      0, // Time slicing unused if all threads have unique priorities     
+                      0,
+                      0, 
+                      0, 
+                      TX_AUTO_START);
+
+    // Create blink thread
+    tx_thread_create( &stThreadBlink, 
+                     "thread_blink", 
+                      thread_blink, 
+                      0, 
+                      auThreadBlinkStack, 
+                      THREAD_BLINK_STACK_SIZE, 
+                      10,
+                      10, 
+                      0, 
                       TX_AUTO_START);
 }
 
@@ -47,16 +67,13 @@ void thread_main(ULONG ctx)
 
     while( true )
     {
-        HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
-        tx_thread_sleep(1000);
-        
         // If MCAN_Rx, update the heart beat enable or disable
         if ( heartbeatFlagPrevious != heartbeatFlag)
         {
             switch(heartbeatFlag)
             {
                 case true:
-                    MCAN_EnableHeartBeats(1000, heartbeatData); 
+                    MCAN_EnableHeartBeats(HEARTBEAT_DELAY_MS, heartbeatData); 
                     break;
 
                 case false:
@@ -65,12 +82,20 @@ void thread_main(ULONG ctx)
             }
             heartbeatFlagPrevious = heartbeatFlag;
         }
+
+        tx_thread_sleep(THREAD_MAIN_DELAY_MS);
     }
+}
+
+void thread_blink(ULONG ctx)
+{
+    HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+    tx_thread_sleep(THREAD_BLINK_DELAY_MS);
 }
 
 void MCAN_Rx_Handler( void )
 {
-    if ( mcanRxMessage.mcanID.MCAN_RX_Device == DEV_MAIN_COMPUTE || mcanRxMessage.mcanID.MCAN_RX_Device == DEV_ALL )
+    if ( mcanRxMessage.mcanID.MCAN_RX_Device == DEV_DEPLOYMENT || mcanRxMessage.mcanID.MCAN_RX_Device == DEV_ALL )
     {
         heartbeatFlag = (bool) mcanRxMessage.mcanData[0];
     } 
